140ef4d9a165d16f5fa749b42882089f3ba0fe23
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-09 21:42:36 -0700

    Brew formula update for kopia version v0.3.0-pre10

--- kopia.rb
+++ kopia.rb
@@ -2,9 +2,9 @@
 class Kopia < Formula
   desc "Fast and secure open source backup."
   homepage "https://kopia.io"
-  url "https://github.com/kopia/kopia/releases/download/v0.3.0-pre9/kopia-0.3.0-pre9-macOS-x64.tar.gz"
-  version "0.3.0-pre9"
-  sha256 "49b489787a36b161b10d544c27c006b4bd86ae09368192037915395ac80143df"
+  url "https://github.com/kopia/kopia/releases/download/v0.3.0-pre10/kopia-0.3.0-pre10-macOS-x64.tar.gz"
+  version "0.3.0-pre10"
+  sha256 "9d3ead0be81726b85f768fd1e67a2f1a701f6449967f8063fc5dd26c4ed86189"
 
   def install
     bin.install "kopia"
