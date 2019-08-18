75db6209a7b5f46ab4b437031f60a174b27b2eaa
goreleaserbot <goreleaser@carlosbecker.com>
2019-08-17 18:45:47 -0700

    Brew formula update for kopia version v0.3.0-rc6

--- kopia.rb
+++ kopia.rb
@@ -2,20 +2,20 @@
 class Kopia < Formula
   desc "Fast and secure open source backup."
   homepage "https://kopia.io"
-  version "0.3.0-rc5"
+  version "0.3.0-rc6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kopia/kopia/releases/download/v0.3.0-rc5/kopia-0.3.0-rc5-macOS-x64.tar.gz"
-    sha256 "396051140fca260741d69993ecdc2625f2f2ee84cbb60e0ac1e9cc57677fef52"
+    url "https://github.com/kopia/kopia/releases/download/v0.3.0-rc6/kopia-0.3.0-rc6-macOS-x64.tar.gz"
+    sha256 "7a770891a259abdbab19e4967a5cc400bcf3861711cb8150dbcbcdd5c7b8cecd"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kopia/kopia/releases/download/v0.3.0-rc5/kopia-0.3.0-rc5-linux-x64.tar.gz"
-      sha256 "4923b0a70408bbfb1696397c22500db74fba8a6945330c430e330e5893fdb85f"
+      url "https://github.com/kopia/kopia/releases/download/v0.3.0-rc6/kopia-0.3.0-rc6-linux-x64.tar.gz"
+      sha256 "6f31a5784982a1738f228d7591977c94de642af3f24dc4fe921122e76e529058"
     end
   end
 
   def install
-    bin.install "program"
+    bin.install "kopia"
   end
 end
