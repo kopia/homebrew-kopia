44def7b7f690fb06ece96049e3538e8df474122c
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-01 18:24:35 -0700

    Brew formula update for kopia version v0.6.4

--- kopia.rb
+++ kopia.rb
@@ -2,24 +2,24 @@
 class Kopia < Formula
   desc "Fast and secure open source backup."
   homepage "https://kopia.io"
-  version "0.6.3"
+  version "0.6.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kopia/kopia/releases/download/v0.6.3/kopia-0.6.3-macOS-x64.tar.gz"
-    sha256 "8a5b7c97ae2ba1f8e197382536028bef6f8e633dd94db5968bc416214f77da44"
+    url "https://github.com/kopia/kopia/releases/download/v0.6.4/kopia-0.6.4-macOS-x64.tar.gz"
+    sha256 "e2aca8d22177abcfff8537c6fb5c440a081dd050266e8fac3fae106f360c156d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kopia/kopia/releases/download/v0.6.3/kopia-0.6.3-linux-x64.tar.gz"
-      sha256 "1f5cbf5326d688b0343f757c4a90905935caed971f90176a195ea0d4aa74f6ab"
+      url "https://github.com/kopia/kopia/releases/download/v0.6.4/kopia-0.6.4-linux-x64.tar.gz"
+      sha256 "e3d872f6ed4cfe3735413a605fc09d1c2eb79abd64b754cf8e918710a6153e71"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/kopia/kopia/releases/download/v0.6.3/kopia-0.6.3-linux-arm64.tar.gz"
-        sha256 "fbe7bc0d20220562c197b7b14b2cb0229bbf1d4ae6a28b92de5615b9eb47fd05"
+        url "https://github.com/kopia/kopia/releases/download/v0.6.4/kopia-0.6.4-linux-arm64.tar.gz"
+        sha256 "03a54ab795664b022ef6a47480e1a02488d76667139f62e330440e609a479623"
       else
-        url "https://github.com/kopia/kopia/releases/download/v0.6.3/kopia-0.6.3-linux-arm.tar.gz"
-        sha256 "c49ac137e7ba15cfacad7286db3d31876b24f49cbf18688d1271b4210ff8cc87"
+        url "https://github.com/kopia/kopia/releases/download/v0.6.4/kopia-0.6.4-linux-arm.tar.gz"
+        sha256 "d8e086f81d3e71e954a196d2e4b0e94fa55bc656a578cb958ec2b92f202dbc5e"
       end
     end
   end
